4357f1e0e10bf8e114ecf28b4d18bee591c03137
EomYerim <dpfladlwkdrn@naver.com>
2025-07-21 14:48:16 +0900

    fix: global error

--- src/main/java/com/cj/cjone/GlobalExceptionHandler.java
+++ src/main/java/com/cj/cjone/GlobalExceptionHandler.java
@@ -1,22 +1,22 @@
-package com.cj.cjone;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<String> handleNullPointer(NullPointerException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("서버 오류: 내부 NullPointerException 발생");
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllExceptions(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("서버 오류: " + ex.getMessage());
-    }
-}
+//package com.cj.cjone;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@RestControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(NullPointerException.class)
+//    public ResponseEntity<String> handleNullPointer(NullPointerException ex) {
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body("서버 오류: 내부 NullPointerException 발생");
+//    }
+//
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body("서버 오류: " + ex.getMessage());
+//    }
+//}
